af3cd456560607c3021aefe055a1855b9a636eaf
littleblueprince <58162001+littleblueprince@users.noreply.github.com>
2021-12-21 21:51:57 +0800

    前端驱动导入

--- src/main/java/com/example/datawarehouse/service/Neo4jService.java
+++ src/main/java/com/example/datawarehouse/service/Neo4jService.java
@@ -1,35 +1,35 @@
-package com.example.datawarehouse.service;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Result;
-import org.neo4j.driver.Session;
-
-import static org.neo4j.driver.Values.parameters;
-public class Neo4jService implements AutoCloseable{
-    private final Driver driver;
-    public Neo4jService( String uri, String user, String password )
-    {
-        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
-    }
-    @Override
-    public void close() throws Exception
-    {
-        driver.close();
-    }
-    public void testquery()
-    {
-        try ( Session session = driver.session() )
-        {
-            Result result = session.run( "MATCH (n:Actor) RETURN n LIMIT 1");
-            System.out.println(result.single().get("n").get("name").asString());
-        }
-    }
-    public static void main( String... args ) throws Exception
-    {
-        try ( Neo4jService greeter = new Neo4jService( "bolt://192.168.1.104:7687", "neo4j", "zjp5683zjp" ) )
-        {
-            greeter.testquery();
-        }
-    }
-}
+//package com.example.datawarehouse.service;
+//import org.neo4j.driver.AuthTokens;
+//import org.neo4j.driver.Driver;
+//import org.neo4j.driver.GraphDatabase;
+//import org.neo4j.driver.Result;
+//import org.neo4j.driver.Session;
+//
+//import static org.neo4j.driver.Values.parameters;
+//public class Neo4jService implements AutoCloseable{
+//    private final Driver driver;
+//    public Neo4jService( String uri, String user, String password )
+//    {
+//        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+//    }
+//    @Override
+//    public void close() throws Exception
+//    {
+//        driver.close();
+//    }
+//    public void testquery()
+//    {
+//        try ( Session session = driver.session() )
+//        {
+//            Result result = session.run( "MATCH (n:Actor) RETURN n LIMIT 1");
+//            System.out.println(result.single().get("n").get("name").asString());
+//        }
+//    }
+//    public static void main( String... args ) throws Exception
+//    {
+//        try ( Neo4jService greeter = new Neo4jService( "bolt://192.168.1.104:7687", "neo4j", "zjp5683zjp" ) )
+//        {
+//            greeter.testquery();
+//        }
+//    }
+//}
