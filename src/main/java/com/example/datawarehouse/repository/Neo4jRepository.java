abbfe6c57245186746475e1606ee4603e43a80f2
zjp-zjp-zjp <914856774@qq.com>
2021-12-26 17:16:56 +0800

    备份一下

--- src/main/java/com/example/datawarehouse/repository/Neo4jRepository.java
+++ src/main/java/com/example/datawarehouse/repository/Neo4jRepository.java
@@ -105,3 +105,23 @@ public class Neo4jRepository implements AutoCloseable {
         return relations;
     }
 }
+//    public ArrayList<Movie> findMovieByImdbRating(float imdbRating){
+//        Session session=driver.session();
+//        ArrayList<Movie> movies = new ArrayList<Movie>();
+//        Result result=session.run("MATCH (m:Movie) WHERE toFloat(m.imdbRating) >= $imdbRating RETURN m",parameters("imdbRating",imdbRating));
+//        for (Result it = result; it.hasNext(); ) {
+//            Record record = it.next();
+//            movies.add(new Movie(record.get("m").get("MediaFormat").asString(),record.get("m").get("Producers").asString(),record.get("m").get("Writers").asString(),record.get("m").get("ASIN").asString(),record.get("m").get("imdbRating").asString(),record.get("m").get("Numberofdiscs").asString(),record.get("m").get("type").asString(),record.get("m").get("title").asString(),record.get("m").get("Studio").asString(),record.get("m").get("MPAArating").asString(),record.get("m").get("Runtime").asString(),record.get("m").get("customer_rating").asString(),record.get("m").get("pages").asString(),record.get("m").get("Language").asString(),record.get("m").get("PackageDimensions").asString(),record.get("m").get("Subtitles").asString(),record.get("m").get("IsDiscontinuedByManufacturer").asString(),record.get("m").get("Director").asString(),record.get("m").get("Actors").asString(),record.get("m").get("AspectRatio").asString(),record.get("m").get("ProductDimensions").asString(),record.get("m").get("Itemmodelnumber").asString(),record.get("m").get("Dubbed").asString(),record.get("m").get("Releasedate").asString()));
+//        }
+//        return movies;
+//    }
+//    public ArrayList<Cooperate> findActorToActor(int count){
+//        Session session=driver.session();
+//        ArrayList<Cooperate> relations = new ArrayList<Cooperate>();
+//        Result result=session.run("MATCH p=(a1:Actor)-[r:actor_cooperate_with_actor]->(a2:Actor) WHERE toInteger(r.count)>$count RETURN a1,a2,r",parameters("count",count));
+//        for (Result it = result; it.hasNext(); ) {
+//            Record record = it.next();
+//            relations.add(new Cooperate(record.get("a1").get("name").asString(), record.get("a2").get("name").asString(), Integer.parseInt(record.get("r").get("count").asString())));
+//        }
+//        return relations;
+//    }
